36b70bb781da6035c826a2b5c1994d10bbc1bf77
Junaida1234456 <146915244+Junaida1234456@users.noreply.github.com>
2023-12-05 12:00:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('Junaid')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+num1=float(input("enter no 1"))
+num2=float(input("enter no 2"))
+result= num1 + num2
+print("SUM OF NUMBERS IS", result)
+print(result)
